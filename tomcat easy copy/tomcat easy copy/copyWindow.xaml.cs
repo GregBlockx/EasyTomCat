9558be58320ce28aca01f51b2058cb5c7c3358ec
GregBlockx <greg.blockx@hotmail.com>
2017-07-22 18:23:31 +0200

    added the project

---
+++ tomcat easy copy/tomcat easy copy/copyWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tomcat_easy_copy
+{
+    /// <summary>
+    /// Interaction logic for copyWindow.xaml
+    /// </summary>
+    public partial class copyWindow : Window
+    {
+        private string sourcePath = @"C:\Users\greg\IdeaProjects";
+        private string targetPath = @"C:\xampp\tomcat\webapps";
+        private string warFileName;
+
+        public copyWindow()
+        {
+            InitializeComponent();
+
+            List<string> directories = Directory.GetDirectories(sourcePath, "*", SearchOption.TopDirectoryOnly).ToList();
+
+            foreach (var dir in directories)
+            {
+                ProjectcomboBox.Items.Add(dir);
+            }
+        }
+
+        private void copyButton_Click(object sender, RoutedEventArgs e)
+        {
+            warFileName = warFileNameTextbox.Text + ".war";
+            sourcePath = ProjectcomboBox.Text;
+
+            string sourceWarFile = addToPath(sourcePath + @"\target\", warFileName);
+            string destWarFile = addToPath(targetPath, warFileName);
+
+            try
+            {
+                System.IO.File.Copy(sourceWarFile, destWarFile, true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private string addToPath(string path, string add)
+        {
+            string newPath = System.IO.Path.Combine(path, add);
+
+            return newPath;
+        }
+
+        private void MenuItemView_Click(object sender, RoutedEventArgs e)
+        {
+            deleteWindow dw = new deleteWindow();
+            dw.Show();
+            this.Close();
+        }
+    }
+}
---
+++ tomcat easy copy/tomcat easy copy/deleteWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tomcat_easy_copy
+{
+    /// <summary>
+    /// Interaction logic for deleteWindow.xaml
+    /// </summary>
+    public partial class deleteWindow : Window
+    {
+        private string path = @"C:\xampp\tomcat\webapps";
+        public deleteWindow()
+        {
+            InitializeComponent();
+
+
+            List<string> directories = Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly).ToList();
+            foreach (var dir in directories)
+            {
+                FoldercomboBox.Items.Add(dir);
+            }
+
+            List<string> files = Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly).ToList();
+            foreach (var file in files)
+            {
+                WarFilecomboBox.Items.Add(file);
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (System.IO.Directory.Exists(FoldercomboBox.Text) && System.IO.File.Exists(WarFilecomboBox.Text))
+            {
+                try
+                {
+                    System.IO.Directory.Delete(FoldercomboBox.Text, true);
+                    System.IO.File.Delete(WarFilecomboBox.Text);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void MenuItemFile_Click(object sender, RoutedEventArgs e)
+        {
+            copyWindow cw = new copyWindow();
+            cw.Show();
+            this.Close();
+        }
+    }
+}
